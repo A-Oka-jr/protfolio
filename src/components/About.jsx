e714e3425f80c958100dc7ae4280765cd05badc4
Ahmed Okasha <75159786+A-Oka-jr@users.noreply.github.com>
2024-05-15 18:45:33 +0400

    portfolio

---
+++ src/components/About.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "./hoc";
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450,
+          }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+        >
+          <img
+            src={icon}
+            alt="web-development"
+            className="w-16 h-16 object-contain"
+          />
+
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        Energetic and adaptable full-stack software developer with 5 years of
+        hands-on experience in designing, developing, and deploying web
+        applications using MERN stack technologies. I am committed to building
+        robust software systems and thrive in collaborative environments,
+        applying software engineering concepts throughout the development
+        lifecycle. I am highly motivated to learn and stay abreast of the latest
+        technologies, ensuring that I bring innovative and cutting-edge
+        solutions to every project.
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
---
+++ src/constants/index.js
@@ -0,0 +1,267 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  maham,
+  lemon,
+  smart,
+  carrent,
+  mega,
+  tripguide,
+  mysql,
+  angular,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Frontend Developer",
+    icon: web,
+  },
+  // {
+  //   title: "React Native Developer",
+  //   icon: mobile,
+  // },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Full Stack Developer",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Angular",
+    icon: angular,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "MySQL",
+    icon: mysql,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+];
+
+const experiences = [
+  {
+    title: "Full Stack Developer",
+    company_name: "Al-Maham Medical Equipment Trading L.L.C",
+    icon: maham,
+    iconBg: "#383E56",
+    date: "August 2024 - January 2024",
+    points: [
+      "Reviewed code, debugged problems, and corrected issues.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Analyzed existing software implementations to identify areas requiring improvement.",
+      "Developed functional databases, applications and servers to support websites on back-end.",
+      "Collaborated with back-end developers to design APIs.",
+      "Utilized NodeJS, ORM and SQL/No-SQL to develop and manage databases.",
+      "Created unit test cases for testing and automation.",
+    ],
+  },
+  {
+    title: "Backend Developer",
+    company_name: "Lemon labs",
+    icon: lemon,
+    iconBg: "#E6DEDD",
+    date: "August 2022 - August 2023",
+    points: [
+      "Design and implement complex features in microservices environment.",
+      "Ccontribute to maintaining a huge codebase with a lot of integrations.",
+      "Evaluated and adopted new tools and technologies to achieve company-level goals.",
+      "Developed and maintained core product services, libraries and frameworks.",
+      "Improved performance and reliability of databases, web services and other integrations.",
+      "Working closely with protocols like GRPC, Sockets, HTTP to deliver messages and to communicate to other services.",
+      "Optimize services performance using concepts and technologies like caching and database indexing.",
+    ],
+  },
+  {
+    title: "Full Stack Developer",
+    company_name: "Smart Node",
+    icon: smart,
+    iconBg: "#383E56",
+    date: "October 2018 - July 2022",
+    points: [
+      "Collaborate with the team to build the backend for the platform using NodeJS and technologies like MySQL including systems like video uploading, exams, teachers, and courses management.",
+      "Implemented the front-end part of the system as an interactive portal to manage statistical charts, and handle data visualizations.",
+      "Estimated project scope and worked with PM team to forecast milestones and timelines.",
+      "Oversaw system testing and validation procedures to automate functionality and establish reliability.",
+      "Assisted project managers in understanding client requirements and planning technical solutions.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Real Estate",
+    description:
+      "Web-based platform that allows users to search, book, and manage property rentals, buying, and selling listings from various providers, providing a convenient and efficient solution for real estate needs. It is not done yet",
+    tags: [
+      {
+        name: "React",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "Nodejs",
+        color: "yellow-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/A-Oka-jr/mern-estate",
+  },
+  {
+    name: "E-Commerce mega shop",
+    description:
+      "mega shop is a revolutionary eCommerce platform designed to revolutionize your shopping experience. mega shop brings the mall to your fingertips, allowing you to browse, shop, and save effortlessly from the comfort of your own home.",
+    tags: [
+      {
+        name: "Angular",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Bootstrap",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Mysql",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "Nodejs",
+        color: "yellow-text-gradient",
+      },
+    ],
+    image: mega,
+    source_code_link: "https://github.com/A-Oka-jr/mega_shop",
+  },
+  {
+    name: "Lemar Medical",
+    description:
+      "Lemar is a cutting-edge platform dedicated to booking appointments for cosmetic surgeries at reputable hospitals and clinics.allowing them to select and schedule cosmetic procedures that meet their needs and aspirations with ease and convenience.",
+    tags: [
+      {
+        name: "Angular",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Bootstrap",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Mysql",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "Nodejs",
+        color: "yellow-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/A-Oka-jr/lemar-medical",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
